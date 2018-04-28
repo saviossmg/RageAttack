e642103df289d66b5b885cb166a1f25a4c2e7de8
saviossmg <saviossmg@gmail.com>
2018-04-27 22:12:23 -0300

    1.0
    Primeira Versao

---
+++ app/src/main/java/com/devops/freakattack/rageattack/Principal.java
@@ -0,0 +1,13 @@
+package com.devops.freakattack.rageattack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
